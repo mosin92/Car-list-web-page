00d8114dfc85f84f90244bb611a0d178255ea5b7
Muhammad Tehsin <tehsin118@gmail.com>
2024-05-16 23:32:57 +0500

    car card update

--- src/components/productCard/productCard.js
+++ src/components/productCard/productCard.js
@@ -4,29 +4,31 @@ import "./productCard.css";
 import Image from "next/image";
 import car1 from "../../../public/assets/images/car.webp";
 import location from "../../../public/assets/images//location.svg";
+import like from "../../../public/assets/icons/heart-2.svg";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
   return (
     <>
-      <div className="product-card">
+      <div className="product-card ">
         <div className="left">
           <Image src={car1} alt="car1" priority />
         </div>
         <div className="right">
           <h6>
-            Citroen C1 <span>VTI 72 S&S Live 3p E6.d</span>
+            {product?.title} <span>{product?.subtitle}</span>
           </h6>
           <label htmlFor="">
             <Image src={location} alt="car1" priority />
-            Grasse (06) - Essence - 2021 manuelle
+            {product?.location}
           </label>
-          <p className="discription">
-            C1, Vti 72 s&s feel, Citadine, 06/2021, 72ch, 4cv, 4 places,
-            Climatisation manuelle, Abs, Esp, Direction assistée, Fermeture
-            centralisée, Bluetooth, 9450 € Citroen C1 VTi 72 S&S Feel, Argent,
-            Citadine, date de mise en circulation (b)
-          </p>
+          <p className="discription">{product?.description}</p>
+          <div className="pricing">
+            <h5>{product?.price} &euro;</h5>
+            <p>{product?.seller}</p>
+          </div>
         </div>
+        <Image src={like} alt="car1" priority className="like-btn" />
+        <button>Voir le détail</button>
       </div>
     </>
   );
