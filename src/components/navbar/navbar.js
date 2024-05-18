ce5c3e0e19cf2d44a07d02d037f692a18c704e8e
Muhammad Tehsin <tehsin118@gmail.com>
2024-05-19 02:34:35 +0500

    close icon removed

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,11 +2,9 @@ import React from "react";
 import "./navbar.css";
 import logo from "../../../public/assets/images/logo.gif";
 import menuImg from "../../../public/assets/icons/menu.svg";
-import close from "../../../public/assets/icons/close.svg";
 import favIcon from "../../../public/assets/icons/heart.svg";
 import timeline from "../../../public/assets/icons/timeline.svg";
 import person from "../../../public/assets/icons/person.svg";
-import { IoMdClose } from "react-icons/io";
 import Image from "next/image";
 import { FiMenu } from "react-icons/fi";
 
@@ -35,7 +33,6 @@ const Navbar = () => {
                 type="text"
                 placeholder="Quelle voiture recherchez-vous ?"
               />
-              <Image src={close} alt=" Logo" width={24} height={24} priority />
             </span>
             <button>Rechercher</button>
           </div>
